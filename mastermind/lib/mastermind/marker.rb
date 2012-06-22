0c668797b346d81a5cf8539eacd2bc956985dd8f
Allan Espinosa <aespinosa@cs.uhicago.edu>
2012-06-21 19:41:54 -0700

    Move Marker class into its own file.

---
+++ mastermind/lib/mastermind/marker.rb
@@ -0,0 +1,30 @@
+module Mastermind
+  class Marker
+    def initialize(code, guess)
+      @code = code
+      @guess = guess
+    end
+
+    def exact_matches
+      4.times.inject(0) do |matches, index|
+        matches + (exact_match?(index) ? 1 : 0 )
+      end
+    end
+
+    def number_matches
+      4.times.inject(0) do |matches, index|
+        matches + (number_match?(index) ? 1 : 0)
+      end
+    end
+
+    private
+    def exact_match?(index)
+      @code[index] == @guess[index]
+    end
+
+    def number_match?(index)
+      @code.include?(@guess[index]) && !exact_match?(index)
+    end
+  end
+end
+
