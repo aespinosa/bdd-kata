c39a03247d8a8d4a00cb3f4f554f1a5730a6e25c
Allan Espinosa <aespinosa@cs.uhicago.edu>
2012-06-21 17:30:53 -0700

    Fix expectations of next scenario

--- mastermind/features/step_definitions/mastermind_steps.rb
+++ mastermind/features/step_definitions/mastermind_steps.rb
@@ -17,10 +17,15 @@ Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
-  game = Mastermind::Game.new(output)
+  game = Mastermind::Game.new output
   game.start
 end
 
 Then /^the game should say "(.*?)"$/ do |message|
   assert_includes output.messages, message
+end
+
+Given /^the secret code is "(.*?)"$/ do |code|
+  game = Mastermind::Game.new output 
+  game.start code
 end
