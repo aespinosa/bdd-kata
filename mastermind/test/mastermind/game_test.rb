eb5572cc4fc49d887ef0cda695196db61befba3e
Allan Espinosa <aespinosa@cs.uhicago.edu>
2012-06-21 18:12:30 -0700

    Refactor unit test to a DSL as well

--- mastermind/test/mastermind/game_test.rb
+++ mastermind/test/mastermind/game_test.rb
@@ -2,21 +2,24 @@ require 'test_helper'
 
 class GameTest < Test::Unit::TestCase
   def test_start_sends_a_welcome_message
-    output = stub_everything('output')
-    game = Game.new(output)
-
     output.expects(:puts).with "Welcome to Mastermind!"
 
     game.start "1234"
   end
 
   def test_start_prompts_for_first_guess
-    output = stub_everything('output')
-    game = Game.new(output)
-
     output.expects(:puts).with "Enter guess:"
 
     game.start "1234"
   end
 
+
+  private
+  def game
+    @game ||= Game.new output
+  end
+
+  def output
+    @output ||= stub_everything('output')
+  end
 end
